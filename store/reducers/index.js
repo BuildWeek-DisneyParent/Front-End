5e80d118fb5630f090ce8576b5c2df3179af14e6
Daniel Ferrer <danfrrr13@gmail.com>
2019-09-23 14:37:10 -0400

    Created reducers and actions directories with files.

---
+++ store/reducers/index.js
@@ -0,0 +1,34 @@
+import {
+    FETCHING_POST_DATA_START,
+    FETCHING_POST_DATA_SUCCESS,
+    FETCHING_POST_DATA_FAILURE,
+    USER_LOGIN_START,
+    USER_LOGIN_SUCCESS,
+    USER_LOGIN_FAILURE
+} from '../actions/';
+
+const initialState = {
+    isFetching: false,
+    isLoggingIn: false,
+    post: {
+        id: null,
+        title: '',
+        content: '',
+    },
+    posts: [],
+    user: {
+        id: null,
+        name: ''
+    }
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCHING_POST_DATA_START:
+            return {
+                isFetching: true
+            };
+        default:
+            return state;
+    }
+}
