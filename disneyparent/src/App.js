cfd46d88a943bc9a68df00b6e79dcda2882d9563
Mary <mary.clayton1@hotmail.com>
2019-09-23 21:58:44 -0700

    redo of disneyparent

--- disneyparent/src/App.js
+++ disneyparent/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import Profile from "./components/Profile"
 import './App.css';
-import ProfilePostRequest from "./components/ProfilePostRequest"
 
 function App() {
   return (
     <div className="App">
-      <SearchRequest />
-    <ProfilePostRequest /> 
+      <Profile />
     </div>
   );
 }
