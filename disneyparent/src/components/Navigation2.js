91ede27d0c8d0ee913e05ca2d412063ae70edc59
Mary <mary.clayton1@hotmail.com>
2019-09-23 22:19:01 -0700

    added components

---
+++ disneyparent/src/components/Navigation2.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Navigation2 = () => {
+    return (
+<div>
+    
+</div>
+    )
+}
+export default Navigation2;
---
+++ disneyparent/src/components/PostRequest.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const PostRequest= () => {
+    return (
+<div>
+    
+</div>
+    )
+}
+export default PostRequest;
