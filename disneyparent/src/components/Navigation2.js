2ed02f63970b0ebfe2d0241eaeae7d3abbf7ac4a
Mary <mary.clayton1@hotmail.com>
2019-09-24 12:21:48 -0700

    styling

--- disneyparent/src/components/Navigation2.js
+++ disneyparent/src/components/Navigation2.js
@@ -1,10 +1,16 @@
 import React from "react";
+import {Navigation2Style} from "./Styling/Nav2Styling";
 
 const Navigation2 = () => {
     return (
-<div>
-    
-</div>
+<Navigation2Style>
+<nav>
+    <button>Post Request</button>
+    <a href="/">View Available Request</a>
+    <a href="/">Accepted Requests</a>
+    <input type="search" placeholder="Find a request..."/>
+</nav>
+</Navigation2Style>
     )
 }
 export default Navigation2;
---
+++ disneyparent/src/components/Styling/Nav2Styling.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+export const Navigation2Style = styled.div `
+button {
+width: 197px;
+height: 38px;
+left: 471px;
+top: 195px;
+font-size: 2rem;
+background: #8BD8D2;
+font-family: 'raleway';
+}
+nav {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    margin: 2%;
+    a{
+        font-size:2rem;
+        text-decoration: none;
+        margin: 0 2%;
+        
+    }
+input {
+font-family: 'raleway';
+width: 194px;
+height: 42px;
+left: 1117px;
+top: 195px;
+border: 1px solid #000000;
+box-sizing: border-box;
+font-size:2rem;
+}
+}
+
+`;
