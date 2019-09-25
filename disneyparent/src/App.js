2be70b5fb7628a1028beda286c2dd155e6107063
evansibok <evansibok@gmail.com>
2019-09-25 05:14:51 +0100

    formatted code properly

--- disneyparent/src/App.js
+++ disneyparent/src/App.js
@@ -10,11 +10,20 @@ import LoginForm from './components/Login';
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" render={props => <Profile {...props} />}/>
-      <Route exact path="/editprofile" render={props => <EditProfile {...props} />}/>
-      
-      <Route exact path="/register" render={props => <RegisterForm {...props} />} />
-      <Route exact path="/login" render={props => <LoginForm {...props} />} />
+      <Route exact
+        path="/"
+        render={props => <Profile {...props} />}
+      />
+      <Route exact
+        path="/editprofile"
+        render={props => <EditProfile {...props} />} />
+
+      <Route exact 
+      path="/register" 
+      render={props => <RegisterForm {...props} />} />
+      <Route exact 
+      path="/login" 
+      render={props => <LoginForm {...props} />} />
     </div>
   );
 }
