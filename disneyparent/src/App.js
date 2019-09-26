ff631fa6fac9d4667217cd453cf278bf934e32c4
evansibok <evansibok@gmail.com>
2019-09-26 13:07:37 +0100

    edited the login and register links in home.js to open in a new tab

--- disneyparent/src/App.js
+++ disneyparent/src/App.js
@@ -3,9 +3,9 @@ import Profile from "./Profile"
 import EditProfile from "./EditProfile"
 import AcceptedCards from "./AcceptedCards"
 import RequestCards from "./RequestCards"
-import {Home} from "./Home";
-import {About} from "./About";
-import {Route} from 'react-router-dom'
+import { Home } from "./Home";
+import { About } from "./About";
+import { Route } from 'react-router-dom'
 import './App.css';
 
 import RegisterForm from './components/Register';
@@ -15,27 +15,27 @@ function App() {
   return (
     <div className="App">
       <Route path="/home" component= {Home}/>
-      <Route path="/ourstory" component= {About}/>
-      <Route path="/logout" component= {LoginForm}/>
+      <Route path="/ourstory" component={About} />
+      <Route path="/logout" component={LoginForm} />
       <Route
         path="/profile"
-        render={() => <Profile />}/>
-        <Route
+        render={() => <Profile />} />
+      <Route
         path="/editprofile"
-        render={() => <EditProfile />}/>
-        <Route
+        render={() => <EditProfile />} />
+      <Route
         path="/acceptedrequests"
-        render={() => <AcceptedCards />}/>
-        <Route
+        render={() => <AcceptedCards />} />
+      <Route
         path="/availablerequests"
-        render={() => <RequestCards />}/>
+        render={() => <RequestCards />} />
 
-<Route exact 
-      path="/register" 
-      render={props => <RegisterForm {...props} />} />
-      <Route exact 
-      path="/login" 
-      render={props => <LoginForm {...props} />} />
+      <Route exact
+        path="/register"
+        render={props => <RegisterForm {...props} />} />
+      <Route exact
+        path="/login"
+        render={props => <LoginForm {...props} />} />
     </div>
   );
 }
